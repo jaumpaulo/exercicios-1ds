62bc3a29e21729fe1591320fe72d93e8e6b9d7cb
João Paulo <joao.filho488@aluno.ce.gov.br>
2025-10-14 14:30:13 +0000

    feito

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,9 @@
 // CRIE UMA LÓGICA AQUI
+let resultado = 0
+for (let i = 0; i <= 10; i++) {
+    resultado += i
+    console.log(resultado)
+}
 
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
