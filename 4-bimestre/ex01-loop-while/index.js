e2eb532a1797cc72a2b5dc16b20ea074411cfe71
João Paulo <joao.filho488@aluno.ce.gov.br>
2025-10-14 14:34:14 +0000

    feito

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,9 @@
 // CRIE UMA LÓGICA AQUI
+let resultado = 0 
+while (resultado < 10) {
+    resultado++
+    console.log(resultado)
+}
 
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
