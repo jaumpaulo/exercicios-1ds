17b100bf479e611ccae78e59ad7eb123f12f91d4
João Paulo <joao.filho488@aluno.ce.gov.br>
2025-11-10 12:46:41 +0000

    feito

--- 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
+++ 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
@@ -1,4 +1,12 @@
 // CRIE UMA LÓGICA AQUI
-
+function boasVindas() {
+    console.log("Bem-vindo(a) ao sistema!")
+}
+let a = 10
+let b = 20
+function soma() {
+    let x = a + b
+    console.log(`Resultado da soma: ${x}`)
+}
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { boasVindas, soma }
